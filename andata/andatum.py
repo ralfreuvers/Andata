23ef2c4921fe1865ddad31a14f4448c0c66acd61
Ralf <ralfreuvers@gmail.com>
2025-02-18 17:09:55 +0100

    andatum renaming

--- andata/andatum.py
+++ andata/andatum.py
@@ -7,12 +7,7 @@ class andatum:
         # load song file into self and store the total waveform
 
         # randomly assign the 5 primary frequency signals from the private method to the five personality traits
-        #                     <---low values--->                  <---high values--->
-        # openness:             mono                                stereo
-        # conscientiousness:    low compression                     high compression 
-        # extraversion:         only low/mid freq                   open to all freq       
-        # agreeableness:        hard compr+high freq spikes         (soft compr +) smooth eq
-        # neuroticism:          normal                              pitch modulation + glitchy distortions
+        # openness, conscientiousness, extraversion, agreeableness, neuroticism
         
     def display_info(self):
         return f"{self.year} {self.brand} {self.model}"
